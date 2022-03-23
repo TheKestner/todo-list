03f5e95fb88de26da1511bdd64abe82d1eaf5754
Zachary Kestner <zachary.kestner@student.awesomeinc.org>
2022-03-23 15:41:18 +0000

    saving progress with testing useState w btn

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, {useState} from 'react';
 import './App.css';
 import axios from 'axios';
 import Input from './components/Input';
-import Status from './components/Status';
+import Status from './components/Statusbtn';
 import Todo from './components/Todo';
 
 export default function App() {
@@ -11,12 +11,30 @@ export default function App() {
   // [inputText, setInputText] = useState("");
   // [todos, setTodos] = useState([]);
   // [status, setStatus] = useState([]);
+  const [todos, setTodos] = useState([]);
+
+ 
+  function addTodo(){
+    //this can't be right.  There should be a way to get the info directly from the input field
+    let newState = todos;
+    const newID = Date.now();
+    //the goal is to get the text from the text box into the text field.
+    //it also should generate a unique ID that should greatest ID+1
+    //status should be active by default this is probably fine to leave as a string
+    newState.push({text:"example todo", id:[newID], status:'active'})
+    setTodos(newState)
+    //setLocalStorage should probably happen here.
+    console.log(todos)
+  }
 
   return (
     <div className="App">
       <header>
       <h1>Todo Lists</h1>
       </header>
+      <button onClick={addTodo}>
+        TEST ME
+      </button>
       <Input />
       <Todo />
       <Status />
@@ -27,3 +45,22 @@ export default function App() {
 
 
 
+//.  function addTodo(){
+//   //this can't be right.  There should be a way to get the info directly from the input field
+//   let newState = toDos;
+//   //the goal is to get the text from the text box into the text field.
+//   //it also should generate a unique ID that should greatest ID+1
+//   //status should be active by default this is probably fine to leave as a string
+//   newState.push({text:"example todo", id:'0', status:'active'})
+//   setToDos(newState)
+//   //setLocalStorage should probably happen here.
+//   console.log(toDos)
+// }/
+
+// {text: 'do laundry',
+// id: '0',
+// status:'active'},
+// {text: 'do something',
+// id: '1',
+// status:'active'
+// }
