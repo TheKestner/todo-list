345b4764ce9196cbb0b1a1aba472a1ce11518843
Zachary Kestner <zachary.kestner@student.awesomeinc.org>
2022-03-24 20:15:41 +0000

    saving progress

--- src/components/Todolist.js
+++ src/components/Todolist.js
@@ -1,15 +1,25 @@
 import React from "react";
-
+import {Container, Button, Row, Col} from 'react-bootstrap'
 
 export default function Todolist({addtodos,todos, text}) {
+
+
     
     return(
-        <>
-       <div> 
+        <Container fluid="md">
+        <Row>
+          <Col> {text} </Col>
+        </Row>
+        <Button variant="success">Complete</Button>{' '}
+        <Button variant="danger">Remove</Button>{' '}
+      </Container>
+    );
+}
+
+
+
+{/* <div> 
        <li> {text} </li> 
        </div>
        <button>Completed</button>
-       <button>Remove</button>
-        </>
-    );
-}
+       <button>Danger</button> */}
