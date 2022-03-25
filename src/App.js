8f04bc789e91a7b29b83e64ffed85b4654f75fbf
Zachary Kestner <zachary.kestner@student.awesomeinc.org>
2022-03-25 13:08:36 +0000

    added local storage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import axios from 'axios';
 import Input from './components/Input';
@@ -7,11 +7,19 @@ import Todo from './components/Todo';
 import "bootstrap/dist/css/bootstrap.css";
 
 export default function App() {
-
   // maybe need SUM STATE?
-  const [inputText, setInputText] = useState("");
   // [status, setStatus] = useState([]);
+  const [inputText, setInputText] = useState("");
   const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    setTodos(JSON.parse(localStorage.getItem("text")));
+  }, []);
+
+  useEffect(() => {
+    //storing input text
+    localStorage.setItem("text", JSON.stringify(todos));
+  }, [todos]);
   
  
   function addTodo(text){
