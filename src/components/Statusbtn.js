e036f75e45f5850d9efcb116370a0eaba68eeb2a
Zachary Kestner <zachary.kestner@student.awesomeinc.org>
2022-03-25 14:05:44 +0000

    saving first step toward counter with list length but not completed status

--- src/components/Statusbtn.js
+++ src/components/Statusbtn.js
@@ -3,14 +3,17 @@ import { Button, ButtonGroup } from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function Status({todos, setTodos}) {
+  let len = todos.length;
 
   const gottaDelete = () => {
     setTodos(todos.filter(todo => todo.id !== todo.id))
   }
 
+
   return(
       <>
       <ButtonGroup aria-label="Basic example">
+        {`total count: ${len}`}
         <Button variant="secondary">All</Button>
         <Button variant="secondary">Active</Button>
         <Button variant="secondary">Completed</Button>
